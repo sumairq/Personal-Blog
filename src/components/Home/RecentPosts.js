5a29402302b82c754975e0de5fa4889670fa2553
sumairq <sumairqaisar992@gmail.com>
2023-10-31 17:41:05 +0500

    Finish homepage

--- src/components/Home/RecentPosts.js
+++ src/components/Home/RecentPosts.js
@@ -18,10 +18,10 @@ const RecentPosts = ({ blogs }) => {
           view all
         </Link>
       </div>
-      <div className="grid grid-cols-3 grid-rows-3 gap-16 mt-16">
+      <div className="grid grid-cols-3 grid-rows-2 gap-16 mt-16">
         {
             sortedBlogs.slice(5,11).map((blog, index) => {
-                return <article className="col-span-1 row-span-1 relative"><BlogLayoutThree blog={blog}/></article>
+                return <article key={index} className="col-span-1 row-span-1 relative"><BlogLayoutThree blog={blog}/></article>
             })
         }
       </div>
