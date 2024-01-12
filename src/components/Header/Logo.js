3cd36ec82c74ca7da43bd5cf74cfa2233dd5e4a6
sumairq <sumairqaisar992@gmail.com>
2024-01-12 15:09:00 +0500

    Add dark mode affordances to logo

--- src/components/Header/Logo.js
+++ src/components/Header/Logo.js
@@ -5,11 +5,11 @@ import React from 'react'
 
 const Logo = () => {
   return (
-    <Link href="/" className='flex items-center text-dark'>
-        <div className='w-16 rounded-full overflow-hidden border border-solid border-dark mr-4'>
+    <Link href="/" className='flex items-center text-dark dark:text-light'>
+        <div className='w-16 rounded-full overflow-hidden border border-solid border-dark dark:border-light  mr-4'>
         <Image src={profileImg} alt="Codebucks" className='w-full h-auto rounded-full'/>
         </div>
-        <span className='font-bold text-xl'>Klass Software</span>
+        <span className='font-bold dark:font-semibold text-xl'>Klass Software</span>
         </Link>
   )
 }
