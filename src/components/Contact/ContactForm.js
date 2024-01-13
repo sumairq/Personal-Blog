794152df0e96c2f887bf1cd846be2bf0685f3798
sumairq <sumairqaisar992@gmail.com>
2024-01-13 13:12:17 +0500

    Add responsiveness to Contact page

--- src/components/Contact/ContactForm.js
+++ src/components/Contact/ContactForm.js
@@ -14,7 +14,7 @@ export default function ContactForm() {
   return (
     <form
       onSubmit={handleSubmit(onSubmit)}
-      className="mt-12 text-xl font-medium leading-relaxed font-in"
+      className="mt-12 text-base xs:text-lg sm:text-xl font-medium leading-relaxed font-in"
     >
       Hello! My name is{' '}
       <input
@@ -46,8 +46,8 @@ export default function ContactForm() {
       rows={3}
        className='w-full outline-none border-0 p-0 mx-2 focus:ring-0 placeholder:text-lg border-b
         border-gray focus:border-gray bg-transparent' />
-      <input type="submit" value="send request" className='mt-8 font-medium inline-block capitalize text-xl py-3 px-8 border-2
-      border-solid border-dark rounded cursor-pointer' />
+      <input type="submit" value="send request" className='mt-8 font-medium inline-block capitalize text-lg sm:text-xl py-2 sm:py-3 px-6 sm:px-8 border-2
+      border-solid border-dark dark:border-light rounded cursor-pointer' />
     </form>
   );
 }
